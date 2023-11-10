54bbfb853f05ddc94e8194a6e25dfa24f753d6b8
Sérgio Borges de Souza <sbintcel123@gmail.com>
2023-11-09 22:18:11 -0300

    validação de formulário

---
+++ 04-formulario/script/validacpf.js
@@ -0,0 +1,16 @@
+document.getElementById("validarButton").
+addEventListener("click", function(){
+    // alert("Deu certo!!!");
+    const cpf = document.getElementById("cpfInput").value;
+
+    if(validaCPF(cpf)){
+        document.getElementById("resultado").textContent = "CPF Válido"
+    }else{
+        document.getElementById("resultado").textContent = "CPF Inválido"
+    }
+    })
+
+    function validaCPF(vrCPF){
+        //Implementar as regras de validação do CPF
+        return vrCPF.length === 11 & !isNaN(vrCPF);
+    }
